b6652bbd760e84a64b815b6105d81f2af80a662d
Nyles Burton, II <ncburton@umes.edu>
2026-06-27 16:30:47 -0400

    Updating resources to use jsdelivr CDN links

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as Three from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.min.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.min.js';
 
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 
@@ -38,7 +38,7 @@ class MainScene{
         document.body.appendChild(this.renderer.domElement);
         this.renderer.setClearColor(0x000000);
 
-        this.scene = new Three.Scene();
+        this.scene = new THREE.Scene();
         
 
         //Vr Set up
@@ -63,8 +63,8 @@ class MainScene{
         this.world.addBody(Ground);
         
         //Lights and sky  
-        this.ambientLight = new Three.AmbientLight(0xffffff, 1);
-        const sun = new Three.DirectionalLight(0xffffff,1);
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 1);
+        const sun = new THREE.DirectionalLight(0xffffff,1);
         sun.position.set(50,100,50)
         sun.castShadow = true;
 
@@ -195,9 +195,9 @@ class MainScene{
                 if(this.otherPlayerModel){
                     mesh = this.otherPlayerModel.clone();
                 }else{
-                    mesh = new Three.Mesh(
-                        new Three.BoxGeometry(1,1,2),
-                        new Three.MeshStandardMaterial({color: 0xff0000})
+                    mesh = new THREE.Mesh(
+                        new THREE.BoxGeometry(1,1,2),
+                        new THREE.MeshStandardMaterial({color: 0xff0000})
                     );
                 }
                 this.scene.add(mesh);
@@ -230,7 +230,7 @@ class MainScene{
 
     async init(){
         //Setting the player Rig
-        this.playerRig = new Three.Group();
+        this.playerRig = new THREE.Group();
         this.scene.add(this.playerRig);
         //loading questions: this can be from a file/hardcoded for now but should be from a user upload or server in the future
         await this.loadQeuestions("/https://cdn.jsdelivr.net/gh/NburtonII/CircuitCircuit@latest/questions.json");
@@ -274,7 +274,7 @@ class MainScene{
     }
 
     loadSkybox(){
-        this.scene.background = new Three.CubeTextureLoader()
+        this.scene.background = new THREE.CubeTextureLoader()
         .setPath('Models/Skybox/')
         .load([
             'px.jpg', 'nx.jpg',
